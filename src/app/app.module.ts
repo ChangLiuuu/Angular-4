bbfbca2c3000f324fb0e4ce7beccb660dc052834
ChangLiuuu <spiritchang@hotmail.com>
2017-06-20 21:56:09 -0700

    boom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import {WarningAlert} from "./warning-alert/warning-alert.component";
+import {WarningAlert} from './warning-alert/warning-alert.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { Assignment2Component } from './assignment2/assignment2.component';
